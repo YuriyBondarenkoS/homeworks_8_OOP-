44a4d9d321f6ce7a784b83ddc5e10cdfffa9edfa
Yuriy Bondarenko <bondarenko-yri@yandex.ru>
2022-06-28 01:55:46 +0300

    task_2

--- OOP_2.py
+++ OOP_2.py
@@ -1,4 +1,72 @@
 
+from pprint import pprint
+
+
+file_name = "C:/Users/ysbondarenko/Desktop/Work/netology/homework/8_ООП_2/OOP_2.txt"
+
+
 def recipe_list_processing(file_name: str) -> dict:
-    file = open(file_name)
-    print(file)
+    cook_book = {}
+    index_line = 0
+    with open(file_name, "r", encoding="utf-8") as file:
+        count_line = sum(1 for line in file)
+
+    with open(file_name, "r", encoding="utf-8") as file_1:
+        list_ingredient = []
+
+        for line in file_1:
+
+            str_item = line[:len(line) - 1]
+
+            if (line.find("|") + 1) != 0:
+                ingredient = {}
+                ingredient_name = line.split("|")[0]
+                ingredient['ingredient_name'] = ingredient_name[:len(
+                    ingredient_name) - 1]
+                ingredient['quantity'] = line.split("|")[1].replace(" ", "")
+                ingredient['measure'] = line.split(
+                    "|")[2].replace("\n", "").replace(" ", "")
+                list_ingredient.append(ingredient)
+
+            else:
+
+                if str_item != '' and not str_item.isdigit() and (str_item.find("|") + 1) == 0:
+                    dish = str_item
+
+            index_line += 1
+
+            if str_item == '' or index_line == count_line:
+                cook_book[dish] = list_ingredient
+                del list_ingredient
+                list_ingredient = []
+
+    return cook_book
+
+
+# pprint(recipe_list_processing(file_name))
+
+
+def get_shop_list_by_dishes(dishes, person_count) -> dict:
+
+    cook_book = recipe_list_processing(file_name)
+    grocery_list = {}
+
+    # pprint(cook_book)
+    for item_dish in dishes:
+        if item_dish in cook_book:
+            for list_item in cook_book.get(item_dish):
+                if not list_item.get('ingredient_name') in grocery_list:
+                    quantity_ingredients = {}
+                    quantity_ingredients['measure'] = list_item.get('measure')
+                    quantity_ingredients['quantity'] = list_item.get(
+                        'quantity')
+                    grocery_list[list_item.get(
+                        'ingredient_name')] = quantity_ingredients
+                else:
+                    grocery_list.get('ingredient_name')
+        else:
+            print(f'Блюда "{item_dish}" нет в списке.')
+    pprint(grocery_list)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
